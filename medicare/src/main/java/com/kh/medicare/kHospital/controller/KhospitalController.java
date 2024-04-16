010d3293d6171d51066bda4579d04cd5e472cb34
(정민) <ming2558@naver.com>
2024-04-16 09:11:21 +0900

    컨트롤러 수정

--- medicare/src/main/java/com/kh/medicare/kHospital/controller/KhospitalController.java
+++ medicare/src/main/java/com/kh/medicare/kHospital/controller/KhospitalController.java
@@ -1,7 +1,16 @@
 package com.kh.medicare.kHospital.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jdk.internal.org.jline.utils.InputStreamReader;
 
 @Controller
 public class KhospitalController {
@@ -16,5 +25,33 @@ public class KhospitalController {
 	@RequestMapping("detail.kh")
 	public String selectKhospitalDetail() {
 		return "kHospital/kHospitalDetailView";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="list.kh", produces = "text/xml; charset=utf-8")
+	public String kHospital(String location) throws IOException {
+		
+		String url = "https://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncListInfoInqire";
+		url += "?serviceKey=" + serviceKey;
+		url += "&Q0=" + URLEncoder.encode("서울특별시", "UTF-8");
+		url += "&numOfRows=20";
+		
+		URL requestUrl = new URL(url);
+		HttpURLConnection urlConnection = (HttpURLConnection)requestUrl.openConnection();
+		urlConnection.setRequestMethod("GET");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		
+		String responseText = "";
+		String line;
+		while((line = br.readLine()) != null) {
+			responseText += line;
+		}
+		
+		br.close();
+		urlConnection.disconnect();
+		
+		return responseText;
+		
 	}
 }
